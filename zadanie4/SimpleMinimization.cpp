65c26884871af4b4f8193f2bee6be75ff86a3dd9
mkapala <michal.kapala99@gmail.com>
2022-12-11 13:55:03 +0100

    Added init ex 4

--- zadanie4/SimpleMinimization.cpp
+++ zadanie4/SimpleMinimization.cpp
@@ -1,11 +1,11 @@
 #include "SimpleMinimization.h"
 
 #include <math.h>
+#include <omp.h>
 #include <stdlib.h>
 #include <sys/time.h>
 
 #include <iostream>
-
 const double DR_SHRINK = 0.8;
 
 using namespace std;
@@ -20,7 +20,8 @@ SimpleMinimization::SimpleMinimization(Function *f, double timeLimit) : Minimiza
     bestV = function->value(bestX, bestY, bestZ);
 
     unsigned long seed = (unsigned long)time(NULL);
-    srand48(seed);
+
+    srand48_r(time(NULL), &randBuffer);
 }
 
 SimpleMinimization::~SimpleMinimization() {}
@@ -29,6 +30,7 @@ void SimpleMinimization::find(double dr_ini, double dr_fin, int idleStepsLimit)
 {
     double v, xnew, ynew, znew, vnew, dr;
     int idleSteps = 0;  // liczba krokow, ktore nie poprawily lokalizacji
+    struct drand48_data randBufferL;
 
     std::cout << "Start " << std::endl;
 
@@ -36,64 +38,90 @@ void SimpleMinimization::find(double dr_ini, double dr_fin, int idleStepsLimit)
     {
         // inicjujemy losowo polozenie startowe w obrebie kwadratu o bokach od min do max
 
-        generateRandomPosition();
-
-        v = function->value(x, y, z);  // wartosc funkcji w punkcie startowym
-
-        idleSteps = 0;
-        dr = dr_ini;
-
-        while ((dr > dr_fin) && (idleSteps < idleStepsLimit))
+#pragma omp parallel firstprivate(randBufferL) private(xnew, ynew, znew, dr, idleSteps, vnew)
         {
-            xnew = x + (drand48() - 0.5) * dr;
-            ynew = y + (drand48() - 0.5) * dr;
-            znew = z + (drand48() - 0.5) * dr;
-
-            // upewniamy sie, ze nie opuscilismy przestrzeni poszukiwania rozwiazania
-            xnew = limitX(xnew);
-            ynew = limitY(ynew);
-            znew = limitZ(znew);
-
-            // wartosc funkcji w nowym polozeniu
-            vnew = function->value(xnew, ynew, znew);
-
-            if (vnew < v)
+#pragma omp critical
             {
-                x = xnew;  // przenosimy sie do nowej, lepszej lokalizacji
-                y = ynew;
-                z = znew;
-                v = vnew;
-                idleSteps = 0;  // resetujemy licznik krokow, bez poprawy polozenia
+                generateRandomPosition();
+                v = function->value(x, y, z);  // wartosc funkcji w punkcie startowym
             }
-            else
-            {
-                idleSteps++;  // nic sie nie stalo
 
-                if (idleSteps > idleStepsLimit)
+            idleSteps = 0;
+            dr = dr_ini;
+
+            while ((dr > dr_fin) && (idleSteps < idleStepsLimit))
+            {
+                double xShift, yShift, zShift;
+                drand48_r(&randBufferL, &xShift);
+                drand48_r(&randBufferL, &yShift);
+                drand48_r(&randBufferL, &zShift);
+
+                xnew = x + (xShift - 0.5) * dr;
+                ynew = y + (yShift - 0.5) * dr;
+                znew = z + (zShift - 0.5) * dr;
+
+                // upewniamy sie, ze nie opuscilismy przestrzeni poszukiwania rozwiazania
+                xnew = limitX(xnew);
+                ynew = limitY(ynew);
+                znew = limitZ(znew);
+
+                // wartosc funkcji w nowym polozeniu
+                vnew = function->value(xnew, ynew, znew);
+
+                if (vnew < v)
                 {
-                    dr *= DR_SHRINK;  // zmniejszamy dr
-                    idleSteps = 0;
+#pragma omp critical
+                    {
+                        x = xnew;  // przenosimy sie do nowej, lepszej lokalizacji
+                        y = ynew;
+                        z = znew;
+                        v = vnew;
+                    }
+
+                    idleSteps = 0;  // resetujemy licznik krokow, bez poprawy polozenia
+                }
+                else
+                {
+                    idleSteps++;  // nic sie nie stalo
+
+                    if (idleSteps > idleStepsLimit)
+                    {
+                        dr *= DR_SHRINK;  // zmniejszamy dr
+                        idleSteps = 0;
+                    }
+                }
+
+            }  // dr wciaz za duze
+
+#pragma omp critical
+            {
+                addToHistory();
+
+                if (v < bestV)
+                {  // znalezlismy najlepsze polozenie globalnie
+                    bestV = v;
+                    bestX = x;
+                    bestY = y;
+                    bestZ = z;
+                    std::cout << "New better position: " << x << ", " << y << ", " << z << " value = " << v << std::endl;
                 }
             }
-        }  // dr wciaz za duze
-
-        addToHistory();
-
-        if (v < bestV)
-        {  // znalezlismy najlepsze polozenie globalnie
-            bestV = v;
-            bestX = x;
-            bestY = y;
-            bestZ = z;
-
-            std::cout << "New better position: " << x << ", " << y << ", " << z << " value = " << v << std::endl;
         }
+
     }  // mamy czas na obliczenia
 }
 
 void SimpleMinimization::generateRandomPosition()
 {
-    x = drand48() * (maxX - minX) + minX;
-    y = drand48() * (maxY - minY) + minY;
-    z = drand48() * (maxZ - minZ) + minZ;
+    double xShift, yShift, zShift;
+    static drand48_data randBufferL;
+    drand48_r(&randBufferL, &xShift);
+    drand48_r(&randBufferL, &yShift);
+    drand48_r(&randBufferL, &zShift);
+
+    // std::cout << "Thread " << omp_get_thread_num() << " xshift " << xShift << std::endl;
+
+    x = xShift * (maxX - minX) + minX;
+    y = yShift * (maxY - minY) + minY;
+    z = zShift * (maxZ - minZ) + minZ;
 }
--- zadanie4/main.cpp
+++ zadanie4/main.cpp
@@ -1,73 +1,76 @@
-#include<sys/times.h>
-#include<sys/time.h>
-#include<omp.h>
-#include<iostream>
-#include "Minimization.h"
+#include <omp.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+#include <iostream>
+
 #include "Function.h"
-#include "SimpleTestFunction.h"
+#include "Minimization.h"
 #include "Point3D.h"
 #include "SimpleMinimization.h"
+#include "SimpleTestFunction.h"
 
-const double CZAS_PRACY = 10000; // w msec
-const bool SHOW_HISTORY = false;
+const double CZAS_PRACY = 10000;  // w msec
+const bool SHOW_HISTORY = true;
 
-double getMsecTime() {
-	struct timeval tf;
-	gettimeofday(&tf, NULL);
-	return tf.tv_sec * 1000 + tf.tv_usec * 0.001;
+double getMsecTime()
+{
+    struct timeval tf;
+    gettimeofday(&tf, NULL);
+    return tf.tv_sec * 1000 + tf.tv_usec * 0.001;
 }
 
-tms getTimes() {
-	struct tms current;
-	times(&current);
-	return current;
+tms getTimes()
+{
+    struct tms current;
+    times(&current);
+    return current;
 }
 
-void showTimers(const struct tms &stopTMS, const struct tms &startTMS,
-		double stop, double start) {
-	std::cout << "PMO: User time           = "
-			<< 10 * (stopTMS.tms_utime - startTMS.tms_utime) << " msec"
-			<< std::endl;
-	std::cout << "PMO: User / real time    = "
-			<< (stopTMS.tms_utime - startTMS.tms_utime) * 1000.0
-					/ (stop - start) << "%" << std::endl;
-	std::cout << "PMO: System time         = "
-			<< (stopTMS.tms_stime - startTMS.tms_stime) * 10 << " msec"
-			<< std::endl;
+void showTimers(const struct tms &stopTMS, const struct tms &startTMS, double stop, double start)
+{
+    std::cout << "PMO: User time           = " << 10 * (stopTMS.tms_utime - startTMS.tms_utime) << " msec" << std::endl;
+    std::cout << "PMO: User / real time    = " << (stopTMS.tms_utime - startTMS.tms_utime) * 1000.0 / (stop - start) << "%"
+              << std::endl;
+    std::cout << "PMO: System time         = " << (stopTMS.tms_stime - startTMS.tms_stime) * 10 << " msec" << std::endl;
 }
 
-void showHistory(Minimization *m) {
-	int size = m->historySize();
-	std::vector<Point3D*> history = m->getHistory();
-	for (int i = 0; i < size; i++) {
-		cout << i << " ---- " << (*history[i]) << endl;
-	}
+void showHistory(Minimization *m)
+{
+    int size = m->historySize();
+    std::vector<Point3D *> history = m->getHistory();
+    for (int i = 0; i < size; i++)
+    {
+        cout << i << " ---- " << (*history[i]) << endl;
+    }
 }
 
-void showBestResultFound(Minimization *m) {
-	Point3D *bestResult = m->getBestPosition();
-	std::cout << "PMO: Best x     = " << bestResult->getX() << std::endl;
-	std::cout << "PMO: Best y     = " << bestResult->getY() << std::endl;
-	std::cout << "PMO: Best z     = " << bestResult->getZ() << std::endl;
-	std::cout << "PMO: Best value = " << m->getBestValueFound() << std::endl;
+void showBestResultFound(Minimization *m)
+{
+    Point3D *bestResult = m->getBestPosition();
+    std::cout << "PMO: Best x     = " << bestResult->getX() << std::endl;
+    std::cout << "PMO: Best y     = " << bestResult->getY() << std::endl;
+    std::cout << "PMO: Best z     = " << bestResult->getZ() << std::endl;
+    std::cout << "PMO: Best value = " << m->getBestValueFound() << std::endl;
 }
 
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
+    Function *function = new SimpleTestFunction();
+    Minimization *minimization = new SimpleMinimization(function, CZAS_PRACY);
 
-	Function *function = new SimpleTestFunction();
-	Minimization *minimization = new SimpleMinimization(function, CZAS_PRACY);
+    struct tms startTMS = getTimes();
+    double start = getMsecTime();
+    minimization->find(0.1, 0.00001, 1500);
+    double stop = getMsecTime();
+    struct tms stopTMS = getTimes();
 
-	struct tms startTMS = getTimes();
-	double start = getMsecTime();
-	minimization->find(0.1, 0.00001, 1500 );
-	double stop = getMsecTime();
-	struct tms stopTMS = getTimes();
+    showBestResultFound(minimization);
+    showTimers(stopTMS, startTMS, stop, start);
 
-	showBestResultFound(minimization);
-	showTimers(stopTMS, startTMS, stop, start);
+    if (SHOW_HISTORY)
+        // showHistory(minimization);
+        std::cout << "History size: " << minimization->historySize() << std::endl;
 
-	if ( SHOW_HISTORY )
-		showHistory(minimization);
-
-	return 0;
+    return 0;
 }
